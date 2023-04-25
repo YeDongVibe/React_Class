20d9324ec8a8f55122a31ec58590d3f874a551a8
YeDongVibe <wkwhstladmss@naver.com>
2023-04-25 11:07:24 +0900

    23.04.25

--- src/04/MyDiv.js
+++ src/04/MyDiv.js
@@ -1,10 +1,12 @@
 import MyDivArticle from "./MyDivArticle";
-import "./MyDiv.css"
+import "./MyDiv.css";
+import style from "./MyDiv.module.css";
 
 const MyDiv = () => {
 
     return (
-        <main className="container">
+        <main className={"container"}>
+            <h1>MyDiv</h1>
             <MyDivArticle />   {/* aname이라는 속성을 가진 MyDivArticle */}
             <MyDivArticle aname = 'MyDiv1'/>
             <MyDivArticle aname = 'MyDiv2'/>
--- src/04/MyDivArticle.js
+++ src/04/MyDivArticle.js
@@ -1,8 +1,15 @@
+//전체적으로 css 적용
+import "./MyDiv.css";
+
+//component에 css 적용 : 스타일 이름(className)주기
+import style from "./MyDiv.module.css";
+
 
 //probs함수를 사용하여 나타내기.
 //const MyDivArticle = (probs) => {
+
 // 매게 변수를 probs 사용하지 않고 오브젝트에 매개변수명을 넣어서 사용.
-const MyDivArticle = ({ aname }) => {
+const MyDivArticle = ({aname}) => {
     //const aname = probs.aname;
 
     let n = (aname === undefined) ? '0' : aname.slice(-1); {/* 삼항 연산자 : aname을 n으로 선언하여 끝부분을 잘라내고 0 삽입*/ }
@@ -10,7 +17,7 @@ const MyDivArticle = ({ aname }) => {
     return (
 
         <article>
-            <header><h1>{aname || 'MyDiv0'}</h1></header> {/*Falsy 연산자 : 아무것도 없을 때 이렇게 쓰면 MyDiv0로 표시된다. */}
+            <header><h1 className = {style.mah1}>{aname || 'MyDiv0'}</h1></header> {/*Falsy 연산자 : 아무것도 없을 때 이렇게 쓰면 MyDiv0로 표시된다. */}
             <ul>
                 <li>{n === '0' ? 'MyDiv0' : 'MyDiv' + n }1</li> {/* 삼항연산자로 이용 */}
                 <li>{'MyDiv' + n }2</li>
