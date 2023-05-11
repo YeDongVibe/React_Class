bfb2e2dc03877775ff635c2c9c6bc581211a7ac6
YeDongVibe <wkwhstladmss@naver.com>
2023-05-11 13:09:03 +0900

    23.05.11 tour

--- src/08/Gallery.js
+++ src/08/Gallery.js
@@ -2,9 +2,30 @@
 // https://apis.data.go.kr/B551011/PhotoGalleryService1/gallerySearchList1?serviceKey=VVOmSStfN2wJSHEf%2BdtpJXmiBOUek6SBLCa3ue%2BfX1qCYUMmwRU13VFK6bcrNTm6XpOMTYs1Z77cRmRno%2BYqkg%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&arrange=A&keyword=%ec%98%81%eb%8f%84&_type=json
 
 import style from './Gallery.module.css';
+import GalleryView from './GalleryView';
 import { useState, useEffect, useRef } from 'react';
 
 const Gallery = () => {
+
+    //let TempTag = itemArr.current.map(())
+    const [data, setData] = useState();
+
+    const getData = (kw) => {
+
+
+        let url = `https://apis.data.go.kr/B551011/PhotoGalleryService1/gallerySearchList1?serviceKey=VVOmSStfN2wJSHEf%2BdtpJXmiBOUek6SBLCa3ue%2BfX1qCYUMmwRU13VFK6bcrNTm6XpOMTYs1Z77cRmRno%2BYqkg%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&arrange=A&keyword=${kw}&_type=json`;
+        console.log("url = ", url);
+
+        fetch(url)
+            .then((resp) => resp.json())
+            //.then((data) => console.log('data', data.response.body.items.item))
+            .then((data) => setData(data.response.body.items.item))
+            .catch((err) => console.log("err = ", err));
+
+
+    }
+
+
 
     //키워드 input
     const txt1 = useRef();
@@ -14,12 +35,19 @@ const Gallery = () => {
         txt1.current.focus();
     }, []);
 
+    //data가 바뀔때 마다 실행
+    useEffect(() => {
+        console.log("data", data);
+    }, [data]);
+
     //확인버튼
     const show = (e) => {
         e.preventDefault();
         if (txt1.current.value === '') return;
         let kw = encodeURI(txt1.current.value);
         console.log(txt1.current.value, kw);
+
+        getData(kw);
     }
 
     //취소버튼
@@ -28,20 +56,23 @@ const Gallery = () => {
     }
 
     return (
-        <article>
-            <header>
-                <h1>📷한국관광공사_관광사진 정보📸</h1>
-                <form>
-                    <div className="grid">
-                        <input ref = {txt1} type="text" id="txt" name="txt" placeholder="키워드를 입력하세요!" required />
+        <main className='container'>
+            <article>
+                <header>
+                    <h2>📷한국관광공사_관광사진 정보📸</h2>
+                    <form>
                         <div className="grid">
-                            <button onClick={(e) => show()}>확인</button>
-                            <button onClick={(e) => showClear()}>취소</button>
+                            <input ref={txt1} type="text" id="txt" name="txt" placeholder="키워드를 입력하세요!" required />
+                            <div className="grid">
+                                <button onClick={(e) => show(e)}>확인</button>
+                                <button onClick={(e) => showClear(e)}>취소</button>
+                            </div>
                         </div>
-                    </div>
-                </form>
-            </header>
-        </article>
+                    </form>
+                </header>
+            </article>
+            {data && <GalleryView data={data} />}
+        </main>
     );
 
 }
---
+++ src/08/GalleryView.js
@@ -0,0 +1,27 @@
+const GalleryView = ({ data }) => {
+
+    const dTag = data.map((data, idx) => 
+       
+        <li key={"data" + idx} className="feed">
+            <h3 className="title">{data.galTitle}</h3>
+            <p className="location">{data.galPhotographyLocation}</p>
+            <img src={data.galWebImageUrl}></img>
+            <div className="cl">
+                <p className="word">{data.galSearchKeyword}</p>
+            </div>
+        </li>
+    );
+
+
+    return (
+        <article>
+            <ul>
+                <div key={data.id} className="view">
+                    {dTag}
+                </div>
+
+            </ul>
+        </article>
+    )
+}
+export default GalleryView;
