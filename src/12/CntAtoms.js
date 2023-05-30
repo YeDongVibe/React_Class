02f8b529abaa91c762da4dd572bb7701508ede21
YeDongVibe <wkwhstladmss@naver.com>
2023-05-30 12:11:15 +0900

    23.05.30 recoil

--- src/12/CntAtoms.js
+++ src/12/CntAtoms.js
@@ -1,8 +1,20 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
-//atom을 부르는 방법
+//atom을 사용 방법
 export const CntAtoms = atom({
     //key값은 유일한 변수명으로 지정해야함. 즉 중복 불가. 전역변수이기에 구별되어야함.
     key : "CntAtoms",
     default : 0
+});
+
+//selector 사용 방법
+export const CntAtomsTwice = selector({
+
+    key : "CntAtomsTwice",
+    //get : ({get} => {~~})는 정해진 형식
+    get : ({get}) => {
+        const cntTwice = get(CntAtoms) * 2;
+        return cntTwice;
+        //return get(CntAtoms) * 2; : 이렇게 쓰는것과 동일한 내용임.
+    } 
 });
--- src/12/CntDisp.js
+++ src/12/CntDisp.js
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
 //useRecoilValue : 변경할 필요가 없을 때 즉 읽기만 할때 사용.
 import { useRecoilValue } from "recoil";
-import {CntAtoms} from "./CntAtoms";
+import {CntAtoms, CntAtomsTwice} from "./CntAtoms";
 
 const CntDisp = () => {
     const cnt = useRecoilValue(CntAtoms);
+    const cnt2 = useRecoilValue(CntAtomsTwice);
     return (
         <article>
             <h2>
-                입력값 : {cnt} //  입력값 2배 : 
+                입력값 : {cnt} //  입력값 2배 : {cnt2} 
             </h2>
             <footer>
                 <Link to="/" >입력페이지로 고고링</ Link>
